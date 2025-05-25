cab0ae142b5243f7bb154ad6fdea6811894e27e0
SevenStone <nr19112004.nik@mail.ru>
2025-05-25 21:39:03 +0300

    change second

--- Zadanie9/Program.cs
+++ Zadanie9/Program.cs
@@ -1,6 +1,13 @@
 ﻿Console.WriteLine("Hello, World!");
 printSome();
+printText();
+
 static void printSome()
 {
     Console.WriteLine("Some text");
+}
+
+static void printText()
+{
+    Console.WriteLine("Some more text");
 }
--- Zadanie9/obj/Debug/net8.0/Zadanie9.AssemblyInfo.cs
+++ Zadanie9/obj/Debug/net8.0/Zadanie9.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zadanie9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e2a6eb0b1f5279bbd05a33e8842374612b62253")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zadanie9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zadanie9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
